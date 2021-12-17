8798d98e2be6b2cf12f5c9ebf2bec3010b925d81
Lunarmagpie <Bambolambo0@gmail.com>
2021-12-16 20:50:45 -0500

    remove IDE specific files

--- .history/stats/signals_20200223164531.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Game_stats
-from teams.models import Team
-
-@receiver(post_save, sender=Team)
-def create_game_stats(sender, instance, created, **kwargs):
-    if created:
-        Game_stats.objects.create(team = instance)
